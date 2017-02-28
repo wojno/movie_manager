a777732319f19e284b782cc9238f8cd9903d641d
Ryan Wojno <wojno2112@gmail.com>
2017-02-27 22:10:26 -0500

    - setup basic integration for React to support UI development   #9

--- webpack/application.js
+++ webpack/application.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Hello from './components/hello';
+
+const App = () => (
+  <MuiThemeProvider>
+    <Hello />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
